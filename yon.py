65f25bbaa3b0ccde6e6b3bccaaf1ab53220b804c
yonata1 <149187606+yonata1@users.noreply.github.com>
2023-12-15 12:19:40 -0500

    Another file

--- yon.py
+++ yon.py
@@ -1,3 +1,19 @@
-n = 40
-for i in range(n):
-   print(("+" * (i * 2 + 1)).center(n * 2 - 1))
+gift_list = []
+while True:
+   action = input("Do you want to add or remove a gift? (Type 'add', 'remove', or 'exit'): ")
+  
+   if action == 'add':
+       gift = input("Enter the gift you want to add: ")
+       gift_list.append(gift)
+   elif action == 'remove':
+       if gift_list:
+           removed_gift = gift_list.pop()
+           print("Removed:",removed_gift)
+       else:
+           print("Gift list is empty.")
+   elif action == 'exit':
+       print("Exiting the program. Your gift list:")
+       print(gift_list)
+       break
+   else:
+       print("Invalid choice. Please enter 'add', 'remove', or 'exit'.")
