b684578cbd68f6c81e1ab096f578b4c1763ba353
yonata1 <149187606+yonata1@users.noreply.github.com>
2023-12-15 12:10:37 -0500

    A new file

--- yon.py
+++ yon.py
@@ -1,6 +1,3 @@
-thisList = ["abc", 34, True, 40, "male", False]
-print("Item at index#1:" , thisList[1])
-print("Last item:", thisList[-1])
-print("Items from i2 to i5:", thisList[2:5])
-print("Items from the beginning to i4:", thisList[:4])
-#print("Items from i2 to the end:", thisList[2:])
+n = 40
+for i in range(n):
+   print(("+" * (i * 2 + 1)).center(n * 2 - 1))
