2cb6655eb7a250c572ba149a287ee74ad0351db9
yonata1 <149187606+yonata1@users.noreply.github.com>
2024-02-15 12:33:02 -0500

    push

--- function.py
+++ function.py
@@ -5,6 +5,6 @@ while True:
     
     l = len(inp)
     if l > 4 and inp[-1] == 'r' and inp[-2] == 'o' and inp[-3] not in ['a', 'e', 'i', 'o', 'u', 'y']:
-        print(inp[:-2] + "or")
+        print(inp[:-2] + "our")
     else:
         print(inp)
